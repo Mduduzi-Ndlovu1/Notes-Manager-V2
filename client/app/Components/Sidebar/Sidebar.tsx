87eebc416de795899a60dc38983918b8f47191d5
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2024-10-29 17:39:08 +0200

    feat: add Sidebar and Profile components with user context and task counter
    - Implemented Sidebar with fixed dimensions and positioned it on the right.
    - Integrated Profile component to display user data from context.
    - Added user greeting with name and avatar image using Next.js Image.
    - Displayed task counter with dynamic styling and hover effects.

--- client/app/Components/Sidebar/Sidebar.tsx
+++ client/app/Components/Sidebar/Sidebar.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Profile from '../Profile/Profile'
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <div className='w-[20rem] mt-[5rem] h-[calc(100%-5rem)]  fixed right-0 top-0 bg-[#f9f9f9] flex flex-col'>
+      <Profile />
+    </div>
   )
 }
 
