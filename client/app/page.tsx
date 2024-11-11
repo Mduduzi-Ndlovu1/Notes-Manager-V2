d2fa613ab5a7629c4bcee603954979c1f6ccae0b
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2024-11-11 12:57:49 +0200

    - Added `Filters` component for task filtering functionality - Display tasks in a responsive grid layout using `TaskItem` component

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,6 +2,7 @@
 import { useTasks } from "@/context/taskContext";
 import useRedirect from "@/hooks/useUserRedirect";
 import Filters from "./Components/Filters/Filters";
+import TaskItem from "./Components/TaskItem/TaskItem";
 
 export default function Home() {
   useRedirect("/login");
@@ -16,7 +17,19 @@ export default function Home() {
           All Tasks
         </h1>
         <Filters/>
-      </div>
+
+      <div></div>
+
+        </div>
+        <div className="pb-[2rem] mt-6 grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-[1.5rem]">
+          {
+            tasks.map((task) => (
+              <TaskItem key={task.id} task={task}/>
+            )
+            )
+          }
+
+        </div>
     </main>
   );
 }
